c7ffe5f0a70b7294ee8ed5fbdbb50e20770966df
Xiaodong Qi <qxd@bupt.edu.cn>
2015-05-21 15:04:57 +0800

    Suppress build warnings under Xcode

--- src/draw/text.cc
+++ src/draw/text.cc
@@ -3,7 +3,7 @@
 namespace yage {
 namespace draw {
 
-Text::Text(std::string text, Font &font) : text(text), color(1.0, 1.0, 1.0, 1.0), font_(&font) {}
+Text::Text(std::string text, Font &font) : font_(&font), color(1.0, 1.0, 1.0, 1.0), text(text) {}
 
 Font &Text::get_font(void) const {
     return *font_;
