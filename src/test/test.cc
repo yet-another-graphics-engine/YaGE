e5b90e75ca1026c612a59ad3665f659511b1c54f
Hugh Wang <hghwng@gmail.com>
2015-05-23 11:54:25 +0800

    Add fuzzy match for tests

--- src/test/test.cc
+++ src/test/test.cc
@@ -1,10 +1,11 @@
 #include "test.h"
 #include <vector>
 #include <ctime>
-void test_init(void);
 using test_callback_t = void (*)(void);
-
 vector<pair<test_callback_t, string>> g_tests;
+using TestIter = decltype(g_tests)::iterator;
+
+void test_init(void);
 
 void sleep_sec(int seconds) {
 #ifdef _WIN32
@@ -14,44 +15,88 @@ void sleep_sec(int seconds) {
 #endif
 }
 
-extern "C" int yage_main()
-{
-  test_init();
-  decltype(g_tests)::iterator test_iter = g_tests.end();
+bool fuzzy_string_match(const string &lazy, const string &full) {
+  auto j = full.begin();
 
-  if (yage::argc == 1) {
-    // run the last test_iter
-    --test_iter;
-  } else if (yage::argc == 2) {
-    // run the specified test_iter
-    char *test_name = yage::argv[1];
-    for (decltype(test_iter) i = g_tests.begin(); i != g_tests.end(); ++i) {
-      if (i->second == test_name) {
-        test_iter = i;
-        break;
-      }
+  for (auto i = lazy.begin(); i != lazy.end(); ++i) {
+    for (; j != full.end(); ++j) {
+      if (*i == *j) goto match_next;
     }
-  }
+    return false;
 
-  if (test_iter == g_tests.end()) {
-    // not found
-    fprintf(stderr, "ERROR: Failed to get the test.\n"
-                    "Usage: %s test_name\n\n", yage::argv[0]);
-    fprintf(stderr, "Availiable tests:\n");
-    for (decltype(test_iter) i = g_tests.begin(); i != g_tests.end(); ++i) {
-      fprintf(stderr, "\t%s\n", i->second.c_str());
+match_next:
+    continue;
+  }
+  return true;
+}
+
+void test_show_list() {
+  fprintf(stderr, "Availiable tests:\n");
+  for (auto i = g_tests.begin(); i != g_tests.end(); ++i) {
+    fprintf(stderr, "\t%s\n", i->second.c_str());
+  }
+}
+
+void test_show_usage() {
+  fprintf(stderr, "Usage:\n");
+  fprintf(stderr, "\t%s test_name\n", yage::argv[0]);
+  fprintf(stderr, "\t%s !\n", yage::argv[0]);
+}
+
+TestIter test_match(const string &lazy_name) {
+  auto test_iter = g_tests.end();
+
+  for (auto i = g_tests.begin(); i != g_tests.end(); ++i) {
+    if (!fuzzy_string_match(lazy_name, i->second)) continue;
+    if (test_iter != g_tests.end()) {
+      // multiple match
+      fprintf(stderr, "ERROR: multiple match of test.\n"
+                      "Candidates: test_%s() and test_%s()\n",
+                      test_iter->second.c_str(), i->second.c_str());
+      return g_tests.end();
     }
-    return -1;
+    test_iter = i;
   }
+  return test_iter;
+}
 
-  fprintf(stderr, "INFO: Running test_%s\n", test_iter->second.c_str());
+void test_run(TestIter test_iter) {
+  fprintf(stderr, "INFO: Running test_%s()\n", test_iter->second.c_str());
   clock_t start_time = clock();
 
   test_iter->first();
 
   fprintf(stderr, "INFO: Test end, CPU time = %.2lfs\n",
                   static_cast<double>(clock() - start_time) / CLOCKS_PER_SEC);
-  return 0;
+}
+
+extern "C" int yage_main() {
+  test_init();
+
+  if (yage::argc == 1) {
+    test_run(--g_tests.end());
+    return 0;
+  }
+
+  if (yage::argc == 2) {
+    string test_name = yage::argv[1];
+    if (test_name == "!") {
+      test_show_list();
+      return 0;
+    }
+
+    auto test_iter = test_match(test_name);
+    if (test_iter != g_tests.end()) {
+      test_run(test_iter);
+      return 0;
+    } else {
+      fprintf(stderr, "ERROR: Failed to match the test.\n");
+      return -1;
+    }
+  }
+
+  test_show_usage();
+  return -2;
 }
 
 /*
@@ -64,8 +109,7 @@ extern "C" int yage_main()
  * 4. Execute "bin/yagetest xxx" to run a specific test
  */
 
-void test_init(void)
-{
+void test_init(void) {
 #define TEST_ADD(x) \
   void test_##x(void); \
   g_tests.push_back(pair<test_callback_t, string>(test_##x, #x))
@@ -73,10 +117,10 @@ void test_init(void)
   TEST_ADD(audio);
   TEST_ADD(dialog);
   TEST_ADD(draw);
-  TEST_ADD(draw_perf);
   TEST_ADD(window_count);
   TEST_ADD(window_fix_size);
   TEST_ADD(window_msg);
   TEST_ADD(window_resize);
+  TEST_ADD(draw_perf);
 }
 
