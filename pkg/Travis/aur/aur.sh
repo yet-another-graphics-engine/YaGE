cd8b989f74cd48d972b35d93d858f1f717f93682
Xiaodong Qi <qxd@bupt.edu.cn>
2015-08-02 13:07:58 +0800

    Add AUR uploading script for Travis CI. [av skip]

---
+++ pkg/Travis/aur/aur.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python aur.py
+echo $DEPLOY_PASSPHRASE | openssl rsa -in deploy.key -out deploy -passin stdin
+chmod 600 deploy
+GIT_SSH=./aurssh git clone ssh+git://aur@aur4.archlinux.org/yage.git/ yage-aur
+mv PKGBUILD .SRCINFO yage-aur/
+cd yage-aur
+git config user.name "Bot of Xiaodong Qi"
+git config user.email qxd@bupt.edu.cn
+git commit -am "Update to `cat .SRCINFO | grep pkgver | cut -c 11-`"
+GIT_SSH=../aurssh git push
+cd ../../../
